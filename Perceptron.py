07e1ce99f904fe488199f386720b8b7b1404e8e6
shivam60 <shivamchawla60@gmail.com>
2017-10-30 13:50:14 +0530

    Random weights added

--- Perceptron.py
+++ Perceptron.py
@@ -1,9 +1,9 @@
 import math
 import pickle
 import numpy as np
-
+import matplotlib.pyplot as plt
 class Perceptron():
-	def  __init__(self,layer_info,target,activation_function='identity',alpha=.1,threshold=0):
+	def  __init__(self,layer_info,target,input_matrix,activation_function='identity',alpha=.1,iterations=100,threshold=0):
 		self.activation_function=activation_function
 		self.threshold=threshold
 		self.alpha=alpha
@@ -12,11 +12,12 @@ class Perceptron():
 		self.a0=[]
 		self.activ_fun=self.activation()
 		self.deriva=self.deriv()
-		self.target=None
 		self.layer_info=layer_info
 		self.delta=[]
 		self.target=target
-
+		self.input_matrix=input_matrix
+		self.error=[]
+		self.iterations=iterations
 		if self.layer_info['hidden']==[0]:
 			val=[self.layer_info['inputs']]+[self.layer_info['outputs']]
 			val1=[self.layer_info['outputs']]
@@ -34,7 +35,8 @@ class Perceptron():
 		self.layer_info['back']=len(self.a0)-1
 		
 		for i in range(0,len(val)-1):
-			self.weight_matrix.append(np.zeros(shape=(val[i],val[i+1])))
+			self.weight_matrix.append(np.random.rand(val[i],val[i+1]))
+#			self.weight_matrix.append(np.zeros(shape=(val[i],val[i+1])))
 		for i in val1:
 			self.bias.append(np.zeros(shape=(1,i)))
 	def deriv(self):
@@ -52,16 +54,13 @@ class Perceptron():
 
 				Entropy=del1*self.delta[i]
 				self.delta[i-1]=np.multiply(self.weight_matrix[i].T,Entropy)
-
-#   			print(self.weight_matrix[i].T,Entropy)
-#   			print(np.multiply(self.weight_matrix[i].T,Entropy))
 	def update_weights(self):
 		for i in range(0,len(self.bias)):
 			self.bias[i]=self.bias[i]+self.delta[i]*self.alpha
 			self.weight_matrix[i]=self.weight_matrix[i]+self.alpha*self.delta[i]*self.a0[i].T
 	#to calculates the error
-	def error_LMS(self,t):
-		return 0.5*np.sum(np.power((self.a0[-1]-t),2))
+	def error_LMS(self):
+		return 0.5*np.sum(np.power((self.a0[-1]-self.target),2))
 	#to display the present weights and bias
 	def display_weights(self):
 		print("Weights\n")
@@ -74,8 +73,8 @@ class Perceptron():
 		for i in self.a0:
 			print(i,'\n')
 	#given he input matrix, this calculates the output of the network
-	def feedforward(self,input_matrix):
-		self.a0[0]=np.array(input_matrix).reshape(1,len(input_matrix))
+	def feedforward(self):
+		self.a0[0]=np.array(self.input_matrix).reshape(1,len(self.input_matrix))
 		for i in range(0,len(self.weight_matrix)):
 			z1=self.a0[i].dot(self.weight_matrix[i])+self.bias[i]
 			self.a0[i+1]=self.activ_fun(z1)
@@ -125,22 +124,31 @@ class Perceptron():
 			print("\n4) bipolar step \n 5) bipolar sigmoid")
 		if nf:
 			return np.vectorize(activ)
-		
+	def iterate(self):
+		for i in range(self.iterations):
+			self.feedforward()
+			self.error.append(self.error_LMS())
+			self.backpropogate()
+			self.update_weights()
+	def plot_error(self):
+		plt.plot(self.error)
+		plt.show()
+
 
 if __name__=="__main__":
 
 	layer_info={
-		'inputs':3,
+		'inputs':2,
 		'outputs':1,
 		'hidden':[2]
 	}
-	nn=Perceptron(layer_info=layer_info,alpha=.9,activation_function='binary sigmoid',target=[1])
-	nn.update_particular_weight(0,[[0.2,-0.3],[0.4,0.1],[-0.5,0.2]])
-	nn.update_particular_weight(1,[[-0.3],[-0.2]])
-	nn.update_particular_bias(0,[-0.4,0.2])
-	nn.update_particular_bias(1,[.1])
+	er=[]
+	nn=Perceptron(layer_info=layer_info,alpha=.9,activation_function='binary sigmoid',input_matrix=[0,1],target=[1])
+	#nn.update_particular_weight(0,[[0.6,-0.1],[-0.3,0.4]])
+	#nn.update_particular_weight(1,[[0.4],[0.1]])
+	#nn.update_particular_bias(0,[.3,.5])
+	#nn.update_particular_bias(1,[-0.2])
+	#nn.display_weights()
+	nn.iterate()
+	nn.plot_error()
 #	nn.display_weights()
-	for i in range(100):
-		nn.feedforward(input_matrix=[1,0,1])
-		nn.backpropogate()
-		nn.update_weights()
